71164d69f942ec1af995e2ae63c26e7e2f69670b
Tony Hua <tony7068@gmail.com>
2022-05-14 18:53:03 -0700

    Complete code for a binary search tree

--- bst_map.h
+++ bst_map.h
@@ -3,8 +3,86 @@
 
 #include <iostream>
 
+using std::cout;
+using std::endl;
 
+template<typename Key, typename Value>
+class BSTMap {
+  // You may add functionality to the Node class if you want to, but you must
+  // not remove any functionality or change any variable/method names!
+  // Ask if you'd like to make a change and you're not sure if it is safe.
+  struct Node {
+    Key key;
+    Value value;
+    Node *left, *right;
 
+    Node(Key key)
+        : key(key), left(nullptr), right(nullptr) {}
+  };
 
+  Node *root;
+
+  void print(Node *cur) const {
+    if (cur != nullptr) {
+      print(cur->left);
+      cout << cur->key << ": " << cur->value << endl;
+      print(cur->right);
+    }
+  }
+
+public:
+  BSTMap() : root(nullptr) {}
+
+  BSTMap(const BSTMap& other) {
+    root = deep_copy(other.root);
+  }
+
+  Node* deep_copy(const Node* root) {
+    if (root == nullptr) {
+      return nullptr;
+    }
+    Node *copy = new Node(root->key);
+    copy->value = root->value;
+    copy->left = deep_copy(root->left);
+    copy->right = deep_copy(root->right);
+    return copy;
+  }
+
+	~BSTMap() {
+		delete_node(root);
+	};
+
+  Value& get_or_insert(const Key& key) {
+		return get_or_insert(key, root);	
+	}
+
+  Value& get_or_insert(const Key& key, Node *&curr) {
+    if (curr == nullptr) {
+			curr = new Node(key);
+			return curr->value;
+		} else if (curr->key == key) {
+			return curr->value;
+		}
+
+		if (key < curr->key) {
+			return get_or_insert(key, curr->left);
+		} else { // key > curr->key
+			return get_or_insert(key, curr->right);
+		}
+	}
+
+	void delete_node(Node *&curr) {
+		if (curr) {
+			delete_node(curr->left);
+			delete_node(curr->right);
+			delete curr;
+		}
+	}
+
+  // You may change this method if you want.
+  void print() const {
+    print(root);
+  }
+};
 
 #endif // _BST_MAP_
